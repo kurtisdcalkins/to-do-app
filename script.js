fea9b6f9efcc1871a7e35a82b8f955d0c9499094
Calkins Kurtis <kurtiscalkins@yahoo.com>
2022-08-02 17:51:54 -0600

    Finalized styling and refactored script.js

--- script.js
+++ script.js
@@ -23,49 +23,36 @@ function changeTasks(text) {
     // Create an input element, assign the input text to the value, and then append it to the taskComponent Div
     let checkBox = document.createElement('input');
     checkBox.setAttribute('type', 'checkbox');
+    checkBox.setAttribute('id', 'checkbox');
     taskComponent.prepend(checkBox);
+
+
+
+    // Create a div for the button elements
+    const buttonDiv = document.createElement('div');
+    buttonDiv.setAttribute('id', 'button-div');
 
     // Create the edit button to go along with the task input
     const editButton = document.createElement('button');
     editButton.innerHTML = 'Edit';
-    taskComponent.appendChild(editButton);
+    editButton.classList = 'edit-item-button';
+    buttonDiv.appendChild(editButton);
     // Create the delete button to go along with the task input
     const deleteButton = document.createElement('button');
     deleteButton.innerHTML = 'Delete';
     deleteButton.classList = 'delete-item-button';
-    taskComponent.appendChild(deleteButton);
+    buttonDiv.appendChild(deleteButton);
+
+    // Add buttonDiv to the taskComponent
+    taskComponent.appendChild(buttonDiv);
 
     // Appends the whole div to the 'task-list' section
     taskList.appendChild(taskComponent);
 
 
-
-    // ------ This section creats the functionality for the Edit and Delete buttons ------
-    // Add event listener to the delete button to remove the task div
-    deleteButton.addEventListener('click', (e) => {
-        taskList.removeChild(taskComponent);
-    });
-
-    // 
-    // Add event listener to the edit button to allow task editing
-    editButton.addEventListener('click', (e) => {
-        taskItem.removeAttribute('readonly');
-        taskItem.focus();
-        // Change the button to a 'Save' button which sets the item to read-only
-        editButton.innerHTML = 'Save';
-        editButton.addEventListener('click', (e) => {
-            taskItem.setAttribute('readonly', 'readonly');
-            // Changes the button back into an 'Edit' button
-            editButton.innerHTML = 'Edit';
-        });
-    });
-
-
-
-
     // ----- This section creates checkbox functionality -----
     // Assign a variable to the completed section
-    const completedDiv = document.querySelector('.completed');
+    const completedDiv = document.querySelector('#completed-tasks');
 
     // Check-off list item
     checkBox.addEventListener('change', (e) => {
@@ -74,7 +61,34 @@ function changeTasks(text) {
         } else {
             taskList.appendChild(taskComponent);
         }
-    })
+    });
+
+    // ------ This section creates the functionality for the Edit and Delete buttons ------
+    // Add event listener to the delete button to remove the task div
+    deleteButton.addEventListener('click', (e) => {
+        if (checkBox.checked) {
+            completedDiv.removeChild(taskComponent);
+        } else {
+            taskList.removeChild(taskComponent);
+        }
+    });
+
+
+    // 
+    // Add event listener to the edit button to allow task editing
+    editButton.addEventListener('click', (e) => {
+        if (editButton.innerText.toLowerCase() == 'edit') {
+            editButton.innerHTML = 'Save';
+            taskItem.removeAttribute('readonly');
+            taskItem.focus();
+        } else {
+            // Change the button to a 'Save' button which sets the item to read-only
+            editButton.innerHTML = 'Edit';
+            taskItem.setAttribute('readonly', 'readonly');
+            taskItem.blur();
+        }
+    });
+
 }
 
 
